c2db36f3a05aa4e5763fe95cbce81dbbec0956a4
sandeep.kamath <sandeep.kamath@robosoftin.com>
2018-10-08 00:10:21 +0530

    Added Instrumented test cases.

---
+++ app/src/androidTest/java/com/sandeep/factsapp/FactsListActivityTest.java
@@ -0,0 +1,44 @@
+package com.sandeep.factsapp;
+
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
+
+import com.sandeep.factsapp.facts_list_module.FactsListActivity;
+import com.sandeep.factsapp.network.RestClient;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static org.hamcrest.core.IsNot.not;
+
+@RunWith(AndroidJUnit4.class)
+public class FactsListActivityTest {
+
+
+    @Rule
+    public ActivityTestRule<FactsListActivity> activityTestRule =
+            new ActivityTestRule<>(FactsListActivity.class);
+
+
+    @Test
+    public void onSuccess() throws InterruptedException {
+        Thread.sleep(5000);
+        onView(withId(R.id.facts_list_view)).check(matches(isDisplayed()));
+        onView(withId(R.id.progress_indicator)).check(matches(not(isDisplayed())));
+        onView(withId(R.id.no_result_view)).check(matches(not(isDisplayed())));
+    }
+
+    @Test
+    public void onFailure() throws InterruptedException {
+        Thread.sleep(5000);
+        onView(withId(R.id.facts_list_view)).check(matches(not(isDisplayed())));
+        onView(withId(R.id.progress_indicator)).check(matches(not(isDisplayed())));
+        onView(withId(R.id.no_result_view)).check(matches(isDisplayed()));
+    }
+
+}
